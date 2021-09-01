1b1dba08cc037d2d24964ff685d0ba563e5c59ea
Raj Gupta <rgm5823@gmail.com>
2021-09-01 22:22:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import LoginRegister from "./components/LoginRegister";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
